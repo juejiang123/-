733105901af0561906357ec89975b37eb9318527
juejiang123 <1282978125@qq.com>
2020-11-30 23:12:26 +0800

    servlet-demo编写

---
+++ src/main/java/cn/web/servlet/ServeletDemo1.java
@@ -0,0 +1,23 @@
+package cn.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo")
+public class ServeletDemo1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("get请求");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        System.out.println("post请求");
+    }
+}
